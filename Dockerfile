951f1143fa75d2fb0eec70ec78b40fc34f77975f
= <=>
2021-08-27 10:54:58 +0530

    adds env variable to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:15
 WORKDIR /app
 COPY package.json .
-COPY package-lock.json .
 RUN npm install
 COPY . .
-EXPOSE 3000
+ENV PORT=3000
+EXPOSE $PORT
 CMD ["npm", "run", "dev"]
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.use("/", (_req, res) => {
-	res.send("hello from express!");
+	res.send("hello from express!!!!");
 });
 
 app.listen(port, () => {
