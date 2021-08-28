e90f2fd0e83cd358275a263753e70865f07fcb6c
= <=>
2021-08-28 10:58:59 +0530

    adds profile-specific docker-compose files

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.use("/", (_req, res) => {
-	res.send("hello from express!");
+	res.send("hello from express!!!");
 });
 
-app.listen(port, () => {
-	console.log(`server listening on port ${port}`);
-});
+app.listen(port, () => console.log(`server listening on port ${port}`));
