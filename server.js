3d813bab76d93fbbaad508199d81b30ba82ff58e
= <=>
2021-08-28 18:54:38 +0530

    adds mongo

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+	.connect("mongodb://root:example@mongo:27017")
+	.then(() => console.log(`connected to db`))
+	.catch(() => console.log(`error connecting to db`));
+
 const port = process.env.port || 3000;
 
 app.use("/", (_req, res) => {
